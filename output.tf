2fbfedff7c46bed61c402c277dd96d302acf2fbe
obervinov <49606362+obervinov@users.noreply.github.com>
2023-07-11 16:12:25 +0400

    fixed output.tf

--- output.tf
+++ output.tf
@@ -8,11 +8,6 @@ output "username" {
   value       = var.username
 }
 
-output "password" {
-  description = "Password for new user"
-  value       = random_password.password.result
-}
-
 output "sshkey" {
   description = "SSH Key fingerprint"
   value       = data.digitalocean_ssh_key.ssh_key.fingerprint
