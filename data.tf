8444b667e3e15983347b5aca5c69823bb17a09d0
obervinov <obervinov@pm.me>
2023-11-28 02:14:53 +0400

    modified:   data.tf

--- data.tf
+++ data.tf
@@ -22,12 +22,10 @@ locals {
   }
 
   default_commands = [
-    # Install docker for all environment
     "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
     "echo \"deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \"$$(. /etc/os-release && echo \"$VERSION_CODENAME\")\" stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
     "DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical sudo apt-get -y update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
     "sudo usermod -aG docker ${var.droplet_username}",
-    # Directory for configuration files provisioner
     "sudo mkdir -p ${var.persistent_data_path}/configs && sudo chown ${var.droplet_username}.terraform ${var.persistent_data_path}/configs && sudo chmod 775 ${var.persistent_data_path}/configs"
   ]
 
