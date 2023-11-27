a9c88fd4faeaca476b3a54a7e4976dcf1eb3ad71
obervinov <obervinov@pm.me>
2023-11-27 11:05:19 +0400

    modified:   data.tf modified:   droplet.tf

--- data.tf
+++ data.tf
@@ -1,3 +1,53 @@
+locals {
+  user_data = <<EOF
+#cloud-config
+users:
+  - name: ${var.droplet_username}
+    groups: ['sudo']
+    sudo: ['ALL=(ALL) NOPASSWD:ALL']
+    ssh-authorized-keys:
+      - ${data.digitalocean_ssh_key.key.public_key}
+  - name: terraform
+    groups: ['sudo']
+    sudo: ['ALL=(ALL) NOPASSWD:ALL']
+    ssh-authorized-keys:
+      - ${data.digitalocean_ssh_key.terraform_key.public_key}
+
+ssh_pwauth: false
+disable_root: true
+package_update: true
+package_upgrade: true
+manage_etc_hosts: true
+manage_resolv_conf: true
+
+resolv_conf:
+  nameservers: ${var.nameserver_ips}
+  searchdomains: ['service.consul']
+  domain: 'consul'
+  options:
+    rotate: true
+    timeout: 1
+
+packages:
+  - 'apt-transport-https'
+  - 'ca-certificates'
+  - 'curl'
+  - 'software-properties-common'
+  - 'net-tools'
+  - 'gpg'
+${var.packages_list != null ? join("\n", formatlist("  - '%s'", var.packages_list)) : ""}
+
+runcmd:
+  # Install docker for all environment
+  - 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg'
+  - 'echo "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \"$$(. /etc/os-release && echo \"$VERSION_CODENAME\")\" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
+  - 'DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical sudo apt-get -y update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin'
+  - 'sudo usermod -aG docker ${var.droplet_username}'
+  # Directory for configuration files provisioner
+  - 'sudo mkdir -p ${var.persistent_data_path}/configs && sudo chown ${var.droplet_username}.terraform ${var.persistent_data_path}/configs && sudo chmod 775 ${var.persistent_data_path}/configs'
+EOF
+}
+
 data "digitalocean_ssh_key" "key" {
   name = var.droplet_username
 }
--- droplet.tf
+++ droplet.tf
@@ -1,6 +1,5 @@
 # Provising resources in DigitalOcean
 # Droplet, reserved IP, DNS record, volume, volume snapshot
-
 resource "digitalocean_droplet" "droplet" {
   name          = "${var.droplet_name}-${var.droplet_size}-${var.droplet_region}"
   image         = var.droplet_image
@@ -13,61 +12,8 @@ resource "digitalocean_droplet" "droplet" {
     data.digitalocean_ssh_key.key.id,
     data.digitalocean_ssh_key.terraform_key.id
   ]
-  tags = var.droplet_tags
-
-  user_data = <<EOF
-#cloud-config
-users:
-  - name: ${var.droplet_username}
-    groups:
-      - sudo
-    sudo:
-      - ALL=(ALL) NOPASSWD:ALL
-    ssh-authorized-keys:
-      - ${data.digitalocean_ssh_key.key.public_key}
-  - name: terraform
-    groups:
-      - sudo
-    sudo:
-      - ALL=(ALL) NOPASSWD:ALL
-    ssh-authorized-keys:
-      - ${data.digitalocean_ssh_key.terraform_key.public_key}
-
-ssh_pwauth: false
-disable_root: true
-package_update: true
-package_upgrade: true
-manage_etc_hosts: true
-manage_resolv_conf: true
-
-resolv_conf:
-  nameservers:
-${join("\n", formatlist("    - %s", var.nameserver_ips))}
-  searchdomains:
-    - consul
-  domain: consul
-  options:
-    rotate: true
-    timeout: 1
-
-packages:
-  - apt-transport-https
-  - ca-certificates
-  - curl
-  - software-properties-common
-  - net-tools
-  - gpg
-${var.packages_list != null ? join("\n", formatlist("  - %s", var.packages_list)) : ""}
-
-runcmd:
-  # Install docker for all environment
-  - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  - echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  - DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical sudo apt-get -y update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-  - sudo usermod -aG docker ${var.droplet_username}
-  # Directory for configuration files provisioner
-  - sudo mkdir -p ${var.persistent_data_path}/configs && sudo chown ${var.droplet_username}.terraform ${var.persistent_data_path}/configs && sudo chmod 775 ${var.persistent_data_path}/configs
-EOF
+  tags      = var.droplet_tags
+  user_data = local.user_data
 }
 
 resource "digitalocean_project_resources" "project" {
