c461e6fc34ecf546d6ee8726a8636e45017d3d02
obervinov <obervinov@pm.me>
2023-11-28 13:51:38 +0400

    fixed cloudinit manage-resolv-conf

--- data.tf
+++ data.tf
@@ -21,11 +21,11 @@ locals {
   }
 
   default_commands = [
+    "sudo mkdir -p ${var.persistent_data_path}/configs && sudo chown ${var.droplet_username}.terraform ${var.persistent_data_path}/configs && sudo chmod 775 ${var.persistent_data_path}/configs",
     "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
     "echo \"deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \"$$(. /etc/os-release && echo \"$VERSION_CODENAME\")\" stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
     "DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical sudo apt-get -y update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
-    "sudo usermod -aG docker ${var.droplet_username}",
-    "sudo mkdir -p ${var.persistent_data_path}/configs && sudo chown ${var.droplet_username}.terraform ${var.persistent_data_path}/configs && sudo chmod 775 ${var.persistent_data_path}/configs"
+    "sudo usermod -aG docker ${var.droplet_username}"
   ]
 
   user_data = <<EOF
@@ -36,7 +36,7 @@ disable_root: true
 package_update: true
 package_upgrade: true
 manage_etc_hosts: true
-manage_resolv_conf: true
+manage-resolv-conf: true
 
 users:
   - name: ${var.droplet_username}
